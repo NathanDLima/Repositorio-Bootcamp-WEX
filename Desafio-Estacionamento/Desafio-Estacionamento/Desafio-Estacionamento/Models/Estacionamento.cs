46c49af383c2ab404c76ba8070143b64bfac1525
Nathan <contato.nathanlima@hotmail.com>
2023-10-20 11:36:50 -0300

    Subindo o desafio de projeto Estacionamento

---
+++ Desafio-Estacionamento/Desafio-Estacionamento/Desafio-Estacionamento/Models/Estacionamento.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_Estacionamento.Models
+{
+    public class Estacionamento
+    {
+        /*private decimal precoInicial = 0;
+        private decimal precoPorHora = 0;*/
+
+        private List<string> veiculos = new List<string>();
+
+        public void AdicionarVeiculo()
+        {
+            Console.WriteLine("Por favor, informe a placa do veículo que deseja adicionar: ");
+            string veiculo = Console.ReadLine();
+
+            veiculos.Add(veiculo);
+
+            Console.WriteLine("Veículo adicionado com sucesso!");
+            Console.Clear();
+        }
+
+        public void RemoverVeiculo(decimal precoInicial, decimal precoPorHora)
+        {
+            
+            Console.WriteLine("Por favor, informe a placa do veículo que deseja remover: ");
+            string veiculo = Console.ReadLine();
+            Console.WriteLine("Agora, informe a quantidade de horas que o veículo ficou estacionado: ");
+            int horas = Convert.ToInt32(Console.ReadLine());
+
+            veiculos.Remove(veiculo);
+
+            decimal valorAPagar = precoInicial + (precoPorHora * horas);
+
+            Console.WriteLine($"O veículo foi removido com sucesso, tendo como valor a pagar: R${valorAPagar}\n");
+            
+        }
+
+        public void ListarVeiculo()
+        {
+            Console.Clear();
+            if (veiculos.Count == 0)
+            {
+                Console.WriteLine("Não há carros no estacionamento\n");
+            }
+            else
+            {
+                Console.WriteLine("Carros no estacionamento: ");
+                foreach (var veiculo in veiculos)
+                {
+                    Console.WriteLine(veiculo);
+                }
+                Console.WriteLine("\n");
+                
+            }
+        }
+    }
+}
---
+++ Desafio-Estacionamento/Desafio-Estacionamento/Desafio-Estacionamento/Program.cs
@@ -0,0 +1,41 @@
+﻿using Desafio_Estacionamento.Models;
+
+Estacionamento estacionamento = new Estacionamento();
+
+Console.WriteLine("Seja bem vindo ao estacionamento!");
+Console.WriteLine("Por favor, informe o valor inicial para o pagamento: ");
+decimal valorInicial = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Agora, informe o valor da hora estacionada: ");
+decimal valorHora = Convert.ToDecimal(Console.ReadLine());
+
+do
+{
+    Console.WriteLine("Escolha qual a operação a ser realizada:\n 1. Adicionar veículo\n 2. Remover veículo\n 3. Listar veículo\n 4. Encerrar sessão\n");
+    int escolha = Convert.ToInt32(Console.ReadLine());
+
+    if(escolha == 4)
+    {
+        break;
+    }
+    else
+    {
+        switch (escolha)
+        {
+            case 1:
+                estacionamento.AdicionarVeiculo();
+                break;
+            case 2:
+                estacionamento.RemoverVeiculo(valorInicial, valorHora);
+                break;
+            case 3:
+                estacionamento.ListarVeiculo();
+                break;
+            default:
+                Console.WriteLine("Escolha uma opção válida!\n");
+                break;
+        }
+    }
+    
+} while (true);
+
